b98ed47ccbabf7fbccc86c034e8fb02e05928056
Akash-2176 <akashthewarrior2176@gmail.com>
2026-07-13 01:25:53 +0530

    Dynamic to static site map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,5 @@
 import type { MetadataRoute } from "next";
+export const dynamic = "force-static";
 import { siteConfig } from "@/lib/site";
 import { services } from "@/data/services";
 import { projects } from "@/data/portfolio";
